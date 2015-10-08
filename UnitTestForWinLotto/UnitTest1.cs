c6b4b9c7ba8c6f7b8272e996623b42a514aa5ac1
Matti Järvensivu <matti.jarvensivu@kolumbus.fi>
2015-10-08 12:46:37 +0300

    yksikkötestejä

---
+++ UnitTestForWinLotto/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForWinLotto
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetWeek()
+        {
+
+            //viittaus oikeaan luokkaan ja metodiin
+            JAMK.IT.WinLotto.Lotto lotto = new JAMK.IT.WinLotto.Lotto();
+            //kutsutaan testattavaa metodia
+            string vk = lotto.GetWeekUnfinished();
+            Assert.AreEqual("41/2015", vk);
+        }
+
+        [TestMethod]
+        public void TestGetWeek2()
+        {
+
+            //viittaus oikeaan luokkaan ja metodiin
+            JAMK.IT.WinLotto.Lotto lotto = new JAMK.IT.WinLotto.Lotto();
+            //kutsutaan testattavaa metodia
+            string vk = lotto.GetWeekFixed();
+            Assert.AreEqual("41/2015", vk);
+        }
+
+
+    }
+}
